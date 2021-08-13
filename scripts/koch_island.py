6133ca5a258d66c44fe24241b91932561c5e3e37
Jeremy Chen <jeremy.cz@gmail.com>
2021-08-13 23:45:25 +1200

    Working implementation of Koch island generator

--- scripts/koch_island.py
+++ scripts/koch_island.py
@@ -1,3 +1,17 @@
+"""
+Examples
+
+--n 2 --F F+FF-FF-F-F+F+FF-F-F+F+FF+FF-F
+--n 4 --w " -F" --F F+F-F-F+F
+--n 2 --w F+F+F+F --F F+f-FF+F+FF+Ff+FF-f+FF-F-FF-Ff-FFF --f ffffff
+--n 4 --w F-F-F-F --F FF-F-F-F-F-F+F
+--n 4 --w F-F-F-F --F FF-F-F-F-FF
+--n 3 --w F-F-F-F --F FF-F+F-F-FF
+--n 4 --w F-F-F-F --F FF-F--F-F
+--n 5 --w F-F-F-F --F F-FF--F-F
+--n 4 --w F-F-F-F --F F-F+F-F-F
+"""
+
 import argparse
 import math
 
@@ -8,57 +22,70 @@ import numpy as np
 
 class Generator:
     def __init__(self, d: float, alpha: float):
-        self.path = list()
-        self.x = 0.0
-        self.y = 0.0
-        self.angle = 0.0
+        self.x = [0.0, 0.0]
+        self.y = [0.0, 0.0]
+        self.angle = 90.0 * math.pi / 180.0
 
         self.d = d
         self.angle_increment = alpha * math.pi / 180.0
 
-        self.path.append((self.x, self.y))
+        _, self.ax = plt.subplots()
 
     def run_command(self, command: str) -> None:
         if command == "F" or command == "f":
-            self.x += self.d * math.cos(self.angle)
-            self.y += self.d * math.sin(self.angle)
+            self.x[0] = self.x[1]
+            self.y[0] = self.y[1]
+            self.x[1] = self.x[0] + self.d * math.cos(self.angle)
+            self.y[1] = self.y[0] + self.d * math.sin(self.angle)
 
             if command == "F":
-                self.path.append((self.x, self.y))
+                self.ax.plot(self.x, self.y, color="k", linewidth=1.0)
+
         elif command == "-":
             self.angle -= self.angle_increment
         elif command == "+":
             self.angle += self.angle_increment
 
-    def get_path(self) -> np.array:
-        return np.array(self.path)
+    def get_axes(self) -> plt.axes:
+        return self.ax
 
 
-def main(w: str, p: str, n: int, step_length: float, angle_increment: float) -> None:
+def main(
+    w: str, F: str, f: str, n: int, step_length: float, angle_increment: float
+) -> None:
+    w = w.strip()
+    F = F.strip()
+    f = f.strip()
+
     # Generate command string
     for _ in range(n):
-        w = w.replace("F", p)
+        if len(f) == 0:
+            w = w.replace("F", F)
+        else:
+            w = w.replace("F", "x")
+            w = w.replace("f", f)
+            w = w.replace("x", F)
 
     generator = Generator(step_length, angle_increment)
 
+    print(w[:10])
     for command in tqdm(w):
         generator.run_command(command)
 
-    coords = generator.get_path()
-    _, ax = plt.subplots()
+    ax = generator.get_axes()
     ax.set_aspect("equal")
     ax.axis("off")
-    ax.plot(coords[:, 0], coords[:, 1], color="k", linewidth=1.0)
     plt.show()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--w", type=str, default="F-F-F-F")
-    parser.add_argument("--p", type=str, default="F-F+F+FF-F-F+F")
+    parser.add_argument("--F", type=str, default="F-F+F+FF-F-F+F")
+    parser.add_argument("--f", type=str, default="")
     parser.add_argument("--n", type=int, default=3)
     parser.add_argument("--d", type=float, default=1.0)
     parser.add_argument("--a", type=float, default=90.0)
     args = parser.parse_args()
 
-    main(args.w, args.p, args.n, args.d, args.a)
+    main(args.w, args.F, args.f, args.n, args.d, args.a)
